4a6e335850e512021be50f3e0acee00db30397ea
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-05 23:23:42 +0700

    CleanUP!

--- src/com/javanese/settings/preferences/Utils.java
+++ src/com/javanese/settings/preferences/Utils.java
@@ -14,7 +14,7 @@
  * under the License.
  */
 
-package com.magnus.settings.preferences;
+package com.javanese.settings.preferences;
 
 import android.app.Activity;
 import android.content.Context;
@@ -39,7 +39,7 @@ import android.view.Surface;
 import android.view.WindowManager;
 
 public final class Utils {
-    private static final String TAG = "MAGNUSSettingsUtils";
+    private static final String TAG = "JAVANESESettingsUtils";
 
     // Device types
     private static final int DEVICE_PHONE = 0;
