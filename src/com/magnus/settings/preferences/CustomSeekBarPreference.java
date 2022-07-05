17f2493936215804d5fbe2f9bdfa598eb07dae27
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-05 23:22:04 +0700

    Rename

--- src/com/magnus/settings/preferences/CustomSeekBarPreference.java
+++ src/com/magnus/settings/preferences/CustomSeekBarPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License
  */
 
-package com.magnus.settings.preferences;
+package com.javanese.settings.preferences;
 
 import android.content.Context;
 import android.content.res.TypedArray;
